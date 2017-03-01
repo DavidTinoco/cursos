4681592106918f81405dbe57cae24fd69db3255e
DavidTinoco <daviid.tc@gmail.com>
2017-03-01 19:28:50 +0100

    Falta el 2 y resolver el problema de Unicode con str

--- cursos.py
+++ cursos.py
@@ -15,4 +15,46 @@ for c in raiz.xpath("//duracion"):
 
 #2
 
-pausa = raw_input
+pausa = raw_input("Pulse intro para continuar")
+
+print len(raiz.xpath("cursos/curso"))
+
+
+#3
+
+mes = raw_input("Indique qué mes desea comenzar un curso (01-12): ")
+
+existe = False
+for c in raiz.xpath("//fecha-de-inicio-previsto"):
+    if c.text.split("-")[1] == mes:
+        existe = True
+        print c.getparent().find("nombre").text
+
+if not existe:
+    print "No hay cursos en el mes indicado"
+
+
+#4
+
+curso = unicode(raw_input("De qué curso desea saber sus teléfonos?: "))
+
+existe = False
+for c in raiz.xpath("//nombre"):
+    if c.text == curso:
+        print c.getparent().find("telefonos").text
+
+if not existe:
+    print "No existe dicho curso"
+    
+#5
+
+categoria = raw_input("Escoja la categoría del curso que desea: ")
+
+existe = False
+for c in raiz.xpath("//categoria"):
+    if c.text == categoria:
+        existe = True
+        print "El curso " + c.getparent().getparent().find("nombre").text + " tiene un horario de " + c.getparent().getparent().find("horario").text + " y su página web es: " + c.getparent().getparent().find("url").text
+
+if not existe:
+    print "No existe dicha categoría"        
